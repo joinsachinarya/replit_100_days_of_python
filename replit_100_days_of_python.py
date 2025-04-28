e5516483b9f7e3b12b29ad7b2226140d04550647
Sachin Arya <67674344+joinsachinarya@users.noreply.github.com>
2025-04-28 07:12:14 +0530

    Create replit_100_days_of_python.py

---
+++ replit_100_days_of_python.py
@@ -0,0 +1,12 @@
+# day10 , math operators
+num1 = int(input("Enter first number: ")) #20
+num2 = int(input("Enter second number: ")) #3
+
+print("Sum:", num1 + num2) #23
+print("Sub:", num1 - num2) #17
+print("Mult:", num1 * num2) #60
+print("Div:", num1 / num2) # 6.666666666666667
+print("Power:", num1**num2) # 8000
+print("Modulo:", num1 % num2) # 2
+print("Whole number div:", num1 // num2) # 6
+# total 7 operators
