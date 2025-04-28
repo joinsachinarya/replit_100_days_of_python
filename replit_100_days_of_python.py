7351f24e478a6d9ac2841e2918dd91f5fd243e07
Sachin Arya <67674344+joinsachinarya@users.noreply.github.com>
2025-04-28 07:37:30 +0530

    day 12, debugging

--- replit_100_days_of_python.py
+++ replit_100_days_of_python.py
@@ -1,3 +1,27 @@
+# day 12, debugging 
+print("100 Days of Code QUIZ")
+print()
+input("How many can you answer correctly? ")
+ans1 = input("What language are we writing in?")
+if ans1 == "python":
+  print("Correct")
+else:
+  print("Nope🙈")
+ans2 = int(input("Which lesson number is this?"))
+if ans2>12:
+  print("We're not quite that far yet")
+elif ans2==12:
+  print("That's right!")
+else:
+  print("We've gone well past that!")
+
+
+
+
+
+
+
+
 # day11, leap year
 isLeapYear = input("Is this a leap year?: ")
 if isLeapYear == "yes": 
